e96e2d7e46879ad0dbf4df1daa9b2b6508a2a657
Luke Frisken <l.frisken@gmail.com>
2016-03-30 17:00:14 +1100

    stub for product inventory model

--- src/models/product_inventory.php
+++ src/models/product_inventory.php
@@ -1,15 +1,87 @@
 <?php
+//TODO: inherit from database object
+//to give generic method for setting a property in the database
 	class InventoryItem
 	{
-		function __construct() {
+
+		//create a new inventory item object
+		function __construct(
+			$primary_key,
+			$name, 
+			$cost_price, 
+			$sale_price, 
+			$stock_level, 
+			$description)
+		{
+
 			
+		}
+
+		function getPrimaryKey()
+		{
+
+		}
+
+
+		function getName()
+		{
+
+		}
+
+		function setName()
+		{
+
+		}
+
+		function getCostPrice()
+		{
+
+		}
+
+		function setCostPrice()
+		{
+
+		}
+
+		function getSalePrice()
+		{
+
+		}
+
+		function setSalePrice()
+		{
+
+		}
+
+		function getStockLevel()
+		{
+
+		}
+
+		function setStockLevel()
+		{
+
 		}
 	}
 
 
 	class ProductInventory
 	{
-		function __construct() {
+		function __construct()
+		{
+
+		}
+
+
+		//Returns an InventoryItem according to its primary key
+		function getInventoryItemByKey($primary_key)
+		{
+
+		}
+
+		//Returns an InventoryItem according to its name
+		function getInventoryItemByName($primary_key)
+		{
 
 		}
 	}
