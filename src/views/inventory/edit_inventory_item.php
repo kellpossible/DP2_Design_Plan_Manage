93dfa708ca7ef432b330995ea0987f1564b67c97
Christine Kunak <christinekunak@hotmail.com>
2016-04-05 23:56:55 +1000

    Change to edit_inventory_item forgot to commit in previous commit, updated to work with controller
    Change to edit_inventory_item forgot to commit in previous commit,
    updated to work with controller, fixed syntax as per view item table and
    added name= to input fields for POST

--- src/views/inventory/edit_inventory_item.php
+++ src/views/inventory/edit_inventory_item.php
@@ -1,24 +1,26 @@
 <?php $this->layout('base::website_layout', ['title' => 'Edit Inventory Item']) 
-/** show a table of items in the inventory */?>
+/** show a table of items in the inventory */
 
-    <form name="edit" action="<?php echo "Inventory\EditItem?item=$itemindex"; ?>" method="post">
+?>
+
+    <form name="edit" action="inventory\edititem?key=<?= $item->getPrimaryKey() ?>" method="post">
     
         <fieldset class="form-group">
             <label for="name">Name</label>
-            <input type="text" class="form-control" id="name" value="<?php $product_inventory->getName(); ?>"/>
+            <input type="text" class="form-control" id="name" name="name" value="<?=$item->getName(); ?>"/>
         
             <label for="desc">Description</label>
-            <input type="text" class="form-control" id="desc" value="<?php $product_inventory->getDescription() ?>"/>
+            <input type="text" class="form-control" id="desc" name="desc" value="<?=$item->getDescription() ?>"/>
         
             <label for="cost">Cost price</label>
-            <input type="text" class="form-control" id="cost" value="<?php $product_inventory->getCostPrice()?>"/>
+            <input type="text" class="form-control" id="cost" name="cost" value="<?=$item->getCostPrice()?>"/>
             
             <label for="sale">Sale price</label>
-            <input type="text" class="form-control" id="sale" value="<?php $product_inventory->getSalePrice() ?>"/>
+            <input type="text" class="form-control" id="sale" name="sale" value="<?=$item->getSalePrice() ?>"/>
             
             <label for="stock">Stock level</label>
-            <input type="text" class="form-control" id="stock" value="<?php $product_inventory->getStockLevel() ?>"/>
+            <input type="text" class="form-control" id="stock" name="stock" value="<?=$item->getStockLevel() ?>"/>
         </fieldset>
         
-        <input type="submit" value="Save Changes" class="btn btn-primary"/>	
+        <input type="submit" value="Save Changes" class="btn btn-primary col-md-offset-10 col-md-2"/>
     </form>
