46592d87bd22df71826636773f0dd184a046a328
Christine Kunak <christinekunak@hotmail.com>
2016-04-05 23:55:24 +1000

    updated files in view for edit_inventory_item, static files and website_layout. added edititem() functionality in inventory controller
    Static files added and website_layout updated to work with bootstrap.
    edititem() functionality added, was able to test the GET via the
    test_controller successfully but havent been able to test the POST yet,
    may need to add in check to determine if the pk exists display this view
    else display something else.

--- src/controllers/inventory_controller.php
+++ src/controllers/inventory_controller.php
@@ -26,7 +26,38 @@ class InventoryController extends Controller
 	/** Form to edit an item in the product inventory */
 	public function EditItem()
 	{
-
+        $itemindex = $_GET['key'];
+        
+        $product_inventory = $this->models['product_inventory'];
+            
+        $item = $product_inventory->getItemByKey($itemindex);
+        
+        if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+		
+			echo $this->templates->render('inventory::edit_inventory_item', ['item' => $item]); 
+		}
+		
+		else if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            
+			$name = $_POST['name'];
+			$description = $_POST['desc'];
+			$cost_price = $_POST['cost'];
+			$sale_price = $_POST['sale'];
+			$stock_level = $_POST['stock'];
+						
+			$item->setName($name);
+			$item->setDescription($description);
+			$item->setCostPrice($cost_price);
+			$item->setSalePrice($sale_price);
+			$item->setStockLevel($stock_level);
+			
+			echo $this->templates->render('inventory::inventory_item_table');
+			
+		}
+		
+		else {
+			//no request made
+		}
 	}
 
 	/** Delete item in the product inventory */
--- src/views/base/website_layout.php
+++ src/views/base/website_layout.php
@@ -1,11 +1,46 @@
+<!DOCTYPE html>
 <html>
-<head>
-	<title><?=$this->e($title)?></title>
-</head>
-<body>
+    <head>
+		<title><?=$this->e($title)?></title>
+		<meta charset="utf-8"/>
+		<meta name="viewport" content="width=device-width, initial-sacle=1.0"/>
+		<!----Bootstrap----->
+		<link href="static/css/bootstrap.min.css" rel="stylesheet"/>
+	</head>
+	<body>
+		<div class="container - fluid">
+			<div class ="row">
+				<div class="col-md-8"><h1> Logo </h1></div>
+				<div class="col-md-4"><br />
+                    <div class="input-group">
+                        <input type="text" class="form-control" placeholder="Search...">
+                        <span class="input-group-btn">
+                            <button class="btn btn-default" type="button">
+                                <span class="glyphicon glyphicon-search" aria-hidden="true"></span>
+                            </button>
+                        </span>
+                    </div>
+                </div>
+			</div>
+			<nav class="navbar navbar-default">
+				<ul class="nav navbar-nav">
+					<li class="active"><a href="index.php">Home</a></li>
+					<li><a href="#">Add Stock</a></li>
+					<li><a href="#">Reports</a></li>
+				</ul>
+			</nav>
 
-<?=$this->section('content')?>
+		<?=$this->section('content')?>
 
-</body>
-
+            
+        <p><b>this is the footer testing layout is functioning</b></p>    
+        
+            
+		</div>
+		<!--jQuery - required for Bootstrap's JavaScript plugins) -->
+		<script src="static/js/jquery.min.js"></script>
+		
+		<!--All Bootstrap plugin files-->
+		<script src="static/js/bootstrap.min.js"></script>
+	</body>
 </html>
