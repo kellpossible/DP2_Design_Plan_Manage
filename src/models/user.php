78c18f4cc240259075d7ccd445a8cdbdca17393d
Luke Frisken <l.frisken@gmail.com>
2016-04-05 16:56:02 +1000

    fleshed out user model and added a mockup user

--- src/models/user.php
+++ src/models/user.php
@@ -1,11 +1,60 @@
 <?php
 class UserList extends TableModel
 {
-
+	protected function generateMockups()
+	{
+		$this->addMockup(new User($this, 0, "test", "Test User", "test123"));
+	}
 }
 
 class User extends ItemModel
 {
-	
+	private $password;
+	private $username;
+	private $full_name;
+
+	function __construct(
+		$user_list,
+		$pk,
+		$username,
+		$full_name,
+		$password
+		)
+	{
+		parent::__construct($user_list, $pk);
+		$this->username = $username;
+		$this->full_name = $full_name;
+		$this->password = $password;
+	}
+
+	public function getUsername()
+	{
+		return $this->username;
+	}
+
+	public function setUsername($username)
+	{
+		$this->username = $username;
+	}
+
+	public function getFullName()
+	{
+		return $this->full_name;
+	}
+
+	public function setFullName($full_name)
+	{
+		return $this->full_name;
+	}
+
+	public function getPassword()
+	{
+		return $this->password;
+	}
+
+	public function setPassword($password)
+	{
+		$this->password = $password;
+	}
 }
 ?>
