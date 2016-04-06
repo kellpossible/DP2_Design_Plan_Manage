f197e92bdeeda1795dc8926ae777b98efddd3aa4
Uros <7671725@student.swin.edu.au>
2016-04-06 21:19:30 +1000

    database testing

--- src/test_database.php
+++ src/test_database.php
@@ -5,11 +5,17 @@ ini_set('display_errors', 1);
 require_once('data/database.php');
 
 $db = openDatabase();
-$db->populateDatabase();
 
-
-echo("".$db->checkTableExists("PRODUCT_INVENTORY"));
-
+//echo("".$db->checkTableExists("PRODUCT_INVENTORY"));
+echo("delete test".$db->deleteRowByColumnValue("PRODUCT_INVENTORY", "STOCK_LEVEL", 2));
+//$db->exec('DELETE FROM PRODUCT_INVENTORY WHERE STOCK_LEVEL="50"');
+$db->insertRow([
+	"NAME"=>"New Item",
+	"COST_PRICE"=>"15",
+	"SALE_PRICE"=>"20",
+	"STOCK_LEVEL"=>"1",
+	"DESCRIPTION"=>"Insert item test"
+	]);
 
 $db->close();
 ?>
