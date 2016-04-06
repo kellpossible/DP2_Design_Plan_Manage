fdccf0a00a315c5b08ad27677894a814c3dc28de
Christine Kunak <christinekunak@hotmail.com>
2016-04-06 15:01:16 +1000

    update to controller class, missed in last commit
    Updated new item and delete item functions, will need to test but have
    tested successfully locally, added comments.

--- src/controllers/inventory_controller.php
+++ src/controllers/inventory_controller.php
@@ -21,15 +21,44 @@ class InventoryController extends Controller
 	public function NewItem()
 	{
 		/* You'll need to check to see if this is a form submission or just a request to get the form (a GET or a POST). In the case of it being a form submission, something like $_POST() to get the values of the form and update the database with the new item*/
+        
+        
+        echo $this->templates->render('inventory::new_inventory_item'); 
+        
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {       
+                $name = $_POST['name'];
+                $description = $_POST['desc'];
+                $cost_price = $_POST['cost'];
+                $sale_price = $_POST['sale'];
+                $stock_level = $_POST['stock'];
+        
+            //new instance of InventoryItem to add new item into the db
+                $item = new InventoryItem($this, 0, $name, $description, $cost_price, $sale_price, $stock_level);
+        
+                $product_inventory = $this->models['product_inventory'];
+                $product_inventory->addItem($item);
+             
+                $item->setName($name);
+                $item->setDescription($description);
+                $item->setCostPrice($cost_price);
+                $item->setSalePrice($sale_price);
+                $item->setStockLevel($stock_level);
+                
+                $this->ViewInventory();
+        }
+        
 	}
 
 	/** Form to edit an item in the product inventory */
+    
+    /** Function checks if get or post is requested, grabs the item from the database depending on the key passed through the URL. If the edit link is clicked from the inventory table view the GET will run and render the edit template view and pass through the item data for the view to access with $this->e($item) */
 	public function EditItem()
 	{
+        //Checking if there is a key paramater in the URL when edititem is accessed.
         if(isset( $_GET['key'])){
            
+            
             $itemindex = $_GET['key'];
-
             $product_inventory = $this->models['product_inventory'];
 
             $item = $product_inventory->getItemByKey($itemindex);
@@ -40,26 +69,33 @@ class InventoryController extends Controller
             }
 
             else if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-
+                
+                
+                //Accessing the POST variables from the form using the html 'name' input
                 $name = $_POST['name'];
                 $description = $_POST['desc'];
                 $cost_price = $_POST['cost'];
                 $sale_price = $_POST['sale'];
                 $stock_level = $_POST['stock'];
 
+                
+                //setting the value on the selected item
                 $item->setName($name);
                 $item->setDescription($description);
                 $item->setCostPrice($cost_price);
                 $item->setSalePrice($sale_price);
                 $item->setStockLevel($stock_level);
 
+                
+                //calling viewinventory function from within this class
                 $this->ViewInventory();
 
             }
 
             else {
                 //no request made
-            } 
+            }
+            
         }
         else {
             $this->ViewInventory();
@@ -69,7 +105,16 @@ class InventoryController extends Controller
 	/** Delete item in the product inventory */
 	public function DeleteItem()
 	{
-
+        if(isset( $_GET['key'])){
+           
+            $itemindex = $_GET['key'];
+            
+            $product_inventory = $this->models['product_inventory'];
+            $item = $product_inventory->getItemByKey($itemindex);
+            $item->deleteItem();
+            
+            $this->ViewInventory();
+        }
 	}
 }
 ?>
