870abc358aa70e0e621b08ea4e3c62cb2659c57d
Christine Kunak <christinekunak@hotmail.com>
2016-04-12 23:15:22 +1000

    added bootstrap modal for delete confirmation
    added bootstrap modal for delete confirmation

--- src/views/inventory/inventory_item_table.php
+++ src/views/inventory/inventory_item_table.php
@@ -25,8 +25,52 @@
                 <td><?=$item->getCostPrice() ?></td>
                 <td><?=$item->getStockLevel() ?></td>
                 <td><?=$item->getDescription() ?></td>
-                <td><a href="/index.php/Inventory/EditItem?key=<?=$item->getPrimaryKey()?>">Edit</a></td>
-                <td><a href="/index.php/Inventory/DeleteItem?key=<?=$item->getPrimaryKey()?>">Delete</a></td>
+                <td><a href="/index.php/Inventory/EditItem?key=<?=$item->getPrimaryKey()?>" class="btn btn-default">Edit</a></td>
+                <td><a href="#myModal" id="delete" class="btn btn-default" data-toggle="modal" data-target="#myModal" data-cost="<?=$item->getCostPrice() ?>" data-id="<?=$item->getPrimaryKey()?>" data-name="<?=$item->getName() ?>" data-desc="<?=$item->getDescription() ?>" data-sale="<?=$item->getSalePrice() ?>">Delete</a></td>
             </tr>
         <?php endforeach ?>
 </table>
+
+<!-- Modal -->
+
+
+
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title" id="myModalLabel">Delete Item</h4>
+            </div>
+            <div class="modal-body">
+                  <p>Name: <span id="name"></span></p>
+                  <p>Description: <span id="desc"></span></p>
+                  <p>Cost price: <span id="cost"></span></p>
+                  <p>Sale price: <span id="sale"></span></p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
+                <a id="linkdelete" class="btn btn-default"> Delete</a>
+            </div>
+        </div>
+    </div>
+</div>
+
+<script>
+    $(document).on("click", "#delete", function () {
+        
+            var key = $(this).data('id');
+            var name =($(this).data('name')); 
+            var cost = $(this).data('cost');        
+            var sale = $(this).data('sale');
+            var desc = $(this).data('desc');
+
+            document.getElementById('name').innerHTML = name;
+            document.getElementById('cost').innerHTML = cost;
+            document.getElementById('sale').innerHTML = sale;
+            document.getElementById('desc').innerHTML = desc;
+        
+            document.getElementById("linkdelete").href = "/index.php/Inventory/DeleteItem?key=" + key;
+        });
+    
+</script>
